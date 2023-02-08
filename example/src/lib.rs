2e81a0a6e11368c70ddf2ed2c6465a2a59742fb3
Matt Hunzinger <matthunz2@gmail.com>
2023-02-08 12:48:42 -0500

    Update example

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,11 +1,10 @@
 use android_activity::AndroidApp;
 use android_intent::{with_current_env, Action, Extra, Intent};
-use jni::JavaVM;
 
 #[no_mangle]
 fn android_main(_android_app: AndroidApp) {
     with_current_env(|env| {
-        Intent::new(env.clone(), Action::Send)
+        Intent::new(env, Action::Send)
             .with_type("text/plain")
             .with_extra(Extra::Text, "Hello World!")
             .create_chooser()
