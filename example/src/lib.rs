70596baed902aa116f7678d57b403ffafb8f6c45
Matt Hunzinger <matthunz2@gmail.com>
2023-02-08 09:22:44 -0500

    Add more methods to Intent

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -4,7 +4,10 @@ use android_intent::{current_vm, Action, Intent};
 #[no_mangle]
 fn android_main(android_app: AndroidApp) {
     let vm = current_vm();
-    Intent::new(&vm, Action::View, "https://google.com").start_activity();
+    Intent::new(&vm, Action::View)
+        .with_type("text/plain")
+        .with_extra("EXTRA_TEXT", "Hello World!")
+        .start_activity();
 
     loop {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,25 @@ pub struct Intent<'env> {
 }
 
 impl<'env> Intent<'env> {
-    pub fn new(vm: &'env JavaVM, action: impl AsRef<str>, uri: impl AsRef<str>) -> Self {
+    pub fn new(vm: &'env JavaVM, action: impl AsRef<str>) -> Self {
+        let env = vm.attach_current_thread().unwrap();
+
+        let intent_class = env.find_class("android/content/Intent").unwrap();
+        let action_view = env
+            .get_static_field(intent_class, action.as_ref(), "Ljava/lang/String;")
+            .unwrap();
+
+        let intent = env
+            .new_object(intent_class, "(Ljava/lang/String;)V", &[action_view.into()])
+            .unwrap();
+
+        Self {
+            env,
+            object: intent,
+        }
+    }
+
+    pub fn new_with_uri(vm: &'env JavaVM, action: impl AsRef<str>, uri: impl AsRef<str>) -> Self {
         let env = vm.attach_current_thread().unwrap();
 
         let url_string = env.new_string(uri).unwrap();
@@ -62,7 +80,7 @@ impl<'env> Intent<'env> {
         }
     }
 
-    pub fn push_data(&self, key: impl AsRef<str>, value: impl AsRef<str>) {
+    pub fn push_extra(&self, key: impl AsRef<str>, value: impl AsRef<str>) {
         let key = self.env.new_string(key).unwrap();
         let value = self.env.new_string(value).unwrap();
 
@@ -76,8 +94,62 @@ impl<'env> Intent<'env> {
             .unwrap();
     }
 
-    pub fn with_data(&self, key: impl AsRef<str>, value: impl AsRef<str>) -> &Self {
-        self.push_data(key, value);
+    pub fn with_extra(&self, key: impl AsRef<str>, value: impl AsRef<str>) -> &Self {
+        self.push_extra(key, value);
+        self
+    }
+
+    pub fn set_chooser(&mut self, title: impl AsRef<str>) -> &mut Self {
+        let jstring = self.env.new_string(title).unwrap();
+
+        let intent_class = self.env.find_class("android/content/Intent").unwrap();
+        let intent = self
+            .env
+            .call_static_method(
+                intent_class,
+                "createChooser",
+                "(Ljava/lang/String;Landroid/net/Uri;)V",
+                &[self.object.into(), jstring.into()],
+            )
+            .unwrap();
+
+        self.object = intent.try_into().unwrap();
+        self
+    }
+
+    pub fn into_chooser(&mut self, title: impl AsRef<str>) -> &mut Self {
+        let jstring = self.env.new_string(title).unwrap();
+
+        let intent_class = self.env.find_class("android/content/Intent").unwrap();
+        let intent = self
+            .env
+            .call_static_method(
+                intent_class,
+                "createChooser",
+                "(Ljava/lang/String;Landroid/net/Uri;)V",
+                &[self.object.into(), jstring.into()],
+            )
+            .unwrap();
+
+        self.object = intent.try_into().unwrap();
+        self
+    }
+
+    pub fn set_type(&self, type_name: impl AsRef<str>) {
+        let jstring = self.env.new_string(type_name).unwrap();
+
+        self.env
+            .call_method(
+                self.object,
+                "setType",
+                "(Ljava/lang/String;)Landroid/content/Intent;",
+                &[jstring.into()],
+            )
+            .unwrap();
+    }
+
+    pub fn with_type(&self, type_name: impl AsRef<str>) -> &Self {
+        self.set_type(type_name);
         self
     }
 
